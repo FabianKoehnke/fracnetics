089bba527e65081f1c0b062744e69de07d25c096
Fabian Köhnke <fabian.koehnke@fonds-advisory.de>
2025-03-26 15:44:13 +0100

    BugFix: missing JNs in  Transition

--- gui/src/graphDataHandler.jsx
+++ gui/src/graphDataHandler.jsx
@@ -120,17 +120,20 @@ export function handleGraphData(nodes, edges) {
                     if(nodes[currentNodeID].data.label.slice(0,2) == "PN"){
                         dataTarget[i] = parseFloat(nodes[currentNodeID].data.nodeValues[0]); 
                         // set values for unused judgment nodes
-                        for(let j=0; j<judgmentNodeFunctions.length;j++){
-                            if(!usedJudgmentNodeFunctions.includes(judgmentNodeFunctions[j])){
-                                nodeFunction = judgmentNodeFunctions[j];
-                                usedJudgmentNodeFunctions.push(nodeFunction);
-                                idx = judgmentNodeFunctions.indexOf(nodeFunction);
-                                randomFloat = getRandomFloat(minValues[idx],maxValues[idx]); // TODO change min and max to given boundary
-                                dataFeatures[i][idx] = randomFloat;
+                        if(judgmentNodeFunctions.length>0){
+                            for(let j=0; j<judgmentNodeFunctions.length;j++){
+                                if(!usedJudgmentNodeFunctions.includes(judgmentNodeFunctions[j])){
+                                    nodeFunction = judgmentNodeFunctions[j];
+                                    usedJudgmentNodeFunctions.push(nodeFunction);
+                                    idx = judgmentNodeFunctions.indexOf(nodeFunction);
+                                    randomFloat = getRandomFloat(minValues[idx],maxValues[idx]); // TODO change min and max to given boundary
+                                    dataFeatures[i][idx] = randomFloat;
+                                }
                             }
-                        }
+                            usedJudgmentNodeFunctions.length = 0; 
+                        } 
+                        
                         currentNodeID = nodes[currentNodeID].data.edges[0].target; // set new node
-                        usedJudgmentNodeFunctions.length = 0; 
                         i++;
                     } else if(nodes[currentNodeID].data.label.slice(0,2) == "JN") {                        
                         nodeFunction = nodes[currentNodeID].data.nodeFunction;
@@ -162,5 +165,5 @@ export function handleGraphData(nodes, edges) {
     let data = runTransitionPath(n);
     console.log('Aktuelle Knoten:', nodes);
     console.log('Aktuelle Kanten:', edges);
-    
+    return data;
   }
