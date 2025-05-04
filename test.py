524f6c3375269575c27e46d35ce4201324172961
Fabian Köhnke <fabian.koehnke@googlemail.com>
2025-05-04 16:07:47 +0200

    ignore test files

--- test.py
+++
@@ -1,8 +0,0 @@
-import gymnasium as gym 
-from testII import Test
-env = gym.make("CartPole-v1")
-print(env)
-print("test")
-
-fabi = Test("Fabian", "Köhnke")
-print(fabi)
--- testII.py
+++
@@ -1,6 +0,0 @@
-class Test:
-    def __init__(self, Name, Nickname) -> None:
-        print(">>> reached MyClass.method")
-        self.Name = Name
-        self.Nickname = Nickname
-
