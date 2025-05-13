a5f515a9f8e7509df7d9ce7287ef38a4c6ac9646
Fabian Köhnke <fabian.koehnke@googlemail.com>
2025-05-13 10:48:22 +0200

    implement population in main

--- main.cpp
+++ main.cpp
@@ -1,24 +1,41 @@
 #include "include/Data.hpp"
+#include "include/Population.hpp"
 #include <chrono>
 
 int main(){
     auto start = std::chrono::high_resolution_clock::now();
     Data main;
-    std::vector<std::vector<float > > data;
-    data = main.readCSV("data/BTCUSD-1m-2025-01.csv");
-    std::cout << "done reading csv file \n";
+    /**
+     * Initializing the population
+     */
+    Population population(123,1,5,5); 
+    for(int i=0; i<population.individuals.size(); i++){
+        auto net = population.individuals[i];
+        std::cout << "---------------------------------" << std::endl;
+        std::cout << "Network: " << i << std::endl;
+        std::cout << "---------------------------------" << std::endl;
+        std::vector<Node> nodes = net.innerNodes;
+        std::cout << "type: " << net.startNode.type << " id: " << net.startNode.id << std::endl;
+        std::cout << "edges: " << net.startNode.edges[0] << std::endl;
+        for(const auto& n : nodes){
+            std::cout << "type: " << n.type << " id: " << n.id << std::endl;
+            std::cout << "edges " << "(" << n.edges.size() << "): ";
+            for(auto& ed : n.edges){
+                std::cout << ed << " ";
+            }
+            std::cout << std::endl;
 
-    double sum=0.0;
-    for (int k = 0; k < 10000; ++k){
-
-        for (int i = 0; i < data.size(); ++i){
-        sum += data[i][0];
-        //std::cout << "k = " << k;
+            std::cout << "boundaries" << "(" << n.boundaries.size() << "): ";
+            for(auto& b: n.boundaries){
+                std::cout << b << " ";
+            }
+            std::cout << std::endl;
+ 
+        }
     }
-    }
+ 
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
-    std::cout << "sum = " << sum;
     std::cout << "done in:" << duration.count() << "sek. \n"; 
     return 0;
 }
