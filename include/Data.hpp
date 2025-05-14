e39c1f8d4f7c7f5a730f7dc56e63f842a9d5dc93
Fabian Köhnke <fabian.koehnke@googlemail.com>
2025-05-14 20:07:24 +0200

    class Data: readCSV, printRows, xySplit

--- include/Data.hpp
+++ include/Data.hpp
@@ -5,36 +5,61 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <span>
 
+// TODO write docstring 
 class Data {
-public:
-    int test;
-    std::vector<std::vector<float> > readCSV(const std::string& filename) {
-        std::vector<std::vector<float> > data;
-        std::ifstream file(filename);
-        std::string line;
+    public:
         
-        if (!file.is_open()) {
-            std::cerr << "Fehler beim Öffnen der Datei!" << std::endl;
-            return data;
-        }
+        std::vector<std::vector<float>> dt;
+        std::vector<std::vector<float*>> X;
+        std::vector<float*> y;
         
-        std::getline(file, line); // skip header
-        while (std::getline(file, line)) {
-            std::vector<float> row;
-            std::stringstream ss(line);
-            std::string cell;
+        void readCSV(const std::string& filename, bool header=true) {
+            std::ifstream file(filename);
+            std::string line;
             
-            while (std::getline(ss, cell, ',')) {
-                row.push_back(std::stof(cell));
+            if (!file.is_open()) {
+                std::cerr << "Error opening the file!" << std::endl;
+                return;
             }
-            data.push_back(row);
+            
+            if(header == true){
+                std::getline(file, line);
+            }
+            while (std::getline(file, line)) {
+                std::vector<float> row;
+                std::stringstream ss(line);
+                std::string cell;
+                
+                while (std::getline(ss, cell, ',')) {
+                    row.push_back(std::stof(cell));
+                }
+                dt.push_back(row);
+            }
+            
+            file.close();
         }
-        
-        file.close();
-        return data;
-    }
-};
 
+        void printRows(int nrows){
+            for(int i=0; i<nrows; i++){
+                for(float val : dt[i]){
+                    std::cout << val << " ";
+                }
+                std::cout << std::endl;
+            }
+        }
+
+        void xySplit(){
+            X.resize(dt.size());// set number of rows in X
+            for(int i=0; i<dt.size(); i++){
+                y.push_back(&dt[i].back());
+                X[i].resize(dt[i].size()-1);// set number of columns in X 
+                for(int k=0; k<dt[i].size()-1; k++){
+                    X[i][k] = &dt[i][k];
+                }
+            }
+        }
+};
 #endif
 
