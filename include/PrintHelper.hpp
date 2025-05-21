c38f1f9486560a4023192488e0262c7bd9f5fac2
Fabian Köhnke <fabian.koehnke@googlemail.com>
2025-05-21 08:41:31 +0200

    add printLine

--- include/PrintHelper.hpp
+++ include/PrintHelper.hpp
@@ -3,6 +3,8 @@
 
 #include <sys/resource.h>
 #include <iostream>
+#include <vector>
+#include <string>
 
 inline void printMemoryUsage(){
     struct rusage usage;
@@ -13,4 +15,17 @@ inline void printMemoryUsage(){
 inline void printLine(){
     std::cout << "---------------------------------" << std::endl;
 }
+
+template <typename T>
+inline void printVec(const std::vector<T>& vec, std::string name){
+    std::cout << name << ": [";
+    for (size_t i = 0; i < vec.size(); ++i) {
+        std::cout << vec[i];
+        if (i < vec.size() - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]" << std::endl;
+}
+
 #endif
