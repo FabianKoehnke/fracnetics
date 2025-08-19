781770b5f22d70100539672b965a2283fab0208e
Fabian Köhnke <fabian.koehnke@googlemail.com>
2025-08-19 12:06:09 +0200

    rename tests

--- tests/functionsPop.py
+++ tests/functionsPop.py
@@ -1,9 +1,14 @@
-import fracnetics 
+import fracnetics as fn
+import pandas as pd 
 import sys
 
 def test_population_function():
 
-    pop = fracnetics.Population(
+    data = pd.read_csv("../fracnetics/data/IRIS.csv")
+    X = data.iloc[:,1:5].values
+    y = data.iloc[:,5].values
+
+    pop = fn.Population(
         seed=42,
         ni=10,
         jn=5,
@@ -17,6 +22,13 @@ def test_population_function():
         pop.setAllNodeBoundaries([1,2,3],[0,0,0])
     except Exception as e:
         print("❌ error in pop.setAllNodeBoundaries()")
+        print(e)
+        sys.exit(1)
+
+    try:
+        pop.callFitness(X,y,10,2,"accuracy",1)
+    except Exception as e:
+        print("❌ error in pop.callFitness()")
         print(e)
         sys.exit(1)
 
