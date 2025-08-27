048e4325132b6ebacc3698e8846423a5b4acab3c
Fabian Köhnke <fabian.koehnke@googlemail.com>
2025-08-27 08:13:49 +0200

    del boundary mutation

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "../include/Population.hpp"
 #include "../include/PrintHelper.hpp"
 #include <chrono>
+#include <vector>
 
 int main(){
     /**
@@ -14,7 +15,7 @@ int main(){
     std::string boundaryMutationType = "normal"; // uniform, networkSigma, normal, edgeSigma, edgeFractal
     bool fractalJudgment = false;
     float probCrossOver = 0.05;
-    int generations = 1000;
+    int generations = 100;
     int generationsNoImprovementLimit = 500;
     int nIndividuals = 300;
     int tournamentSize = 2;
@@ -41,6 +42,9 @@ int main(){
     printLine();
     data.columnSelector(std::pair<int, int> (0,0), std::pair<int, int> (0,4)); // set the indices of y and X
     data.minMaxFeatures(); // calculate the min and max values of X (for node boundaries)
+    // for cartpole just empty data needed
+    std::vector<std::vector<double>> X;
+    std::vector<double> y;
     printVec(data.minX, "minX");
     printVec(data.maxX, "miny");
     /**
@@ -62,40 +66,13 @@ int main(){
     int improvementCounter = 0;
     for(int g=0; g<generations; g++){
         //generator = std::make_shared<std::mt19937_64>(5494+g);
-        population.callFitness(data.dt, data.yIndices, data.XIndices, dMax, penalty, "cartpole", maxConsecutiveP);
+        population.callFitness(X, y, dMax, penalty, "cartpole", maxConsecutiveP);
         population.tournamentSelection(tournamentSize,nElite);
         population.callEdgeMutation(probEdgeMutationInnerNodes, probEdgeMutationStartNode);
-
-        std::function<void(Node&, const Population::mutationBoundaryParam&)> boundaryMutation;
-        if(boundaryMutationType == "normal"){
-            boundaryMutation = [&](Node& node, const Population::mutationBoundaryParam& param) {
-                    population.callBoundaryMutationNormal(node, param);
-            };
-         }else if(boundaryMutationType == "uniform"){
-            boundaryMutation = [&](Node& node, const Population::mutationBoundaryParam& param) {
-                    population.callBoundaryMutationUniform(node, param);
-            };
-        }else if(boundaryMutationType == "networkSigma"){
-            boundaryMutation = [&](Node& node, const Population::mutationBoundaryParam& param) {
-                    population.callBoundaryMutationNetworkSizeDependingSigma(node, param);
-            };
-        }else if (boundaryMutationType == "edgeSigma") {
-             boundaryMutation = [&](Node& node, const Population::mutationBoundaryParam& param) {
-                    population.callBoundaryMutationEdgeSizeDependingSigma(node, param);
-            };
-         }else if (boundaryMutationType == "edgeFractal") {
-             boundaryMutation = [&](Node& node, const Population::mutationBoundaryParam& param) {
-                    population.callBoundaryMutationFractal(node, param);
-            };
-        }
-        
-        population.applyBoundaryMutation(
-                {probBoundaryMutation, sigmaBoundaryMutationNormal, 0, data.minX, data.maxX}, boundaryMutation
-        );
-
+       
         population.crossover(probCrossOver);
         if(addDel == 1){
-            population.callAddDelNodes(data.minX, data.maxX, "fractal"); //TODO change type!!
+            population.callAddDelNodes(data.minX, data.maxX);
         }
         std::cout << 
             "Geneation: " << g << 
@@ -104,35 +81,6 @@ int main(){
             " MinFitness: " << population.minFitness <<
             " NetworkSize Best Ind: " << population.individuals[population.indicesElite[0]].innerNodes.size() << std::endl;
 
-        //std::cout << "Best Fit: " << population.individuals[population.indicesElite[0]].fitness;
-        
-        /*
-        for(auto bestIndices : population.indicesElite){
-            std::cout << "indicesElite: " << bestIndices;
-        }
-        std::cout << std::endl;
-        
-        for(int i=0; i<population.individuals.size(); i++){
-            auto& net = population.individuals[i];
-            printLine(); 
-            std::cout << "Generation: " << g << " Network: " << i << " Fit: " << net.fitness << std::endl;
-            printLine(); 
-            std::cout << "type: " << net.startNode.type << " id: " << net.startNode.id << " edges: " << net.startNode.edges[0] << std::endl;
-            for(const auto& n : net.innerNodes){
-                std::cout << "type: " << n.type << " id: " << n.id << " F: " << n.f << " ";
-                std::cout << "edges " << "(" << n.edges.size() << "): ";
-                for(auto& ed : n.edges){
-                    std::cout << ed << " ";
-                }
-
-                std::cout << "boundaries" << "(" << n.boundaries.size() << "): ";
-                for(auto& b: n.boundaries){
-                    std::cout << b << " ";
-                }
-                std::cout << std::endl;
-            }
-        }
-        */
        bestFitnessPerGeneration.push_back(population.bestFit);
        if(g > 1 && bestFitnessPerGeneration[g-1] == bestFitnessPerGeneration[g]){
            improvementCounter++;
@@ -186,7 +134,7 @@ int main(){
     std::cout << "Validation" << std::endl;
     for(int t=0; t<tests; t++){
         //generator = std::make_shared<std::mt19937_64>(54+t);
-        population.callFitness(data.dt, data.yIndices, data.XIndices, dMax, penalty, "cartpole", maxConsecutiveP);
+        population.callFitness(X, y, dMax, penalty, "cartpole", maxConsecutiveP);
         //std::cout << "Best Network: " << " Fit: " << population.individuals[population.indicesElite[0]].fitness << std::endl;
         sumTestFitness += population.individuals[population.indicesElite[0]].fitness; 
     }
