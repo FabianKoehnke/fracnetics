a6d4a2566495a1c9ed3dadcf8e849fdc33aa0f04
Fabian Köhnke <fabian.koehnke@googlemail.com>
2025-12-19 10:10:07 +0100

    test swapOverhandNodes() -> networksize and node ids

--- tests/crossover.cpp
+++ tests/crossover.cpp
@@ -1,5 +1,6 @@
 #include "../include/Population.hpp"
 #include <gtest/gtest.h>
+#include <unordered_map>
 #include <vector>
 
 std::vector<int> storeGenMaterial(Network individual){
@@ -62,6 +63,43 @@ TEST(PopulationTest, BasicAssertions) {
     succesorsFound = populationProcessingLoop.findSuccessorNodes(ind1, 6);
     succesorsTrue = {6};
     EXPECT_EQ(succesorsFound, succesorsTrue);
+
+    // Testing swap overhand 
+    Population populationDiffNetworkSizes(
+        123, // seed 
+        2, // number of networks
+        0, // number of judgment nodes (jn)
+        0, // number of jn functions 
+        10, // number of processing nodes (pn)
+        2, // number of pn functions
+        false
+        ); 
+    ind1 = populationDiffNetworkSizes.individuals[0];
+    Network ind2 = populationDiffNetworkSizes.individuals[1];
+    ind2.innerNodes.erase(ind2.innerNodes.begin());
+    for(Node& node : ind2.innerNodes){node.id --;}
+
+    // assert: changed networkes sizes  
+    int size1 = ind1.innerNodes.size();
+    int size2 = ind2.innerNodes.size();
+    std::vector<int> successor1 = {1,2,3};
+    std::vector<int> successor2 = {1,2};
+    std::unordered_map<int, int> swapMap1 = {{2,1}, {3,2}};
+    populationDiffNetworkSizes.swapOverhangNodes(successor1, successor2, ind1, ind2, swapMap1);
+    EXPECT_EQ(size1, ind2.innerNodes.size());
+    EXPECT_EQ(size2, ind1.innerNodes.size());
+
+    // assert: consecutive node ids 
+    std::vector<int> idsInd1True; for(int i=0; i<ind1.innerNodes.size(); i++){idsInd1True.push_back(i);}
+    std::vector<int> idsInd1; for(int i=0; i<ind1.innerNodes.size(); i++){idsInd1.push_back(ind1.innerNodes[i].id);}
+    EXPECT_EQ(idsInd1True, idsInd1);
+    std::vector<int> idsInd2True; for(int i=0; i<ind2.innerNodes.size(); i++){idsInd2True.push_back(i);}
+    std::vector<int> idsInd2; for(int i=0; i<ind2.innerNodes.size(); i++){idsInd2.push_back(ind2.innerNodes[i].id);}
+    EXPECT_EQ(idsInd2True, idsInd2);
+   
+    // assert: new edge mapping
+    
+
 
 
 
